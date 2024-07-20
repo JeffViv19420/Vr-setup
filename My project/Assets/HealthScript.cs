0dc7ad579bdc646d4e3e90cc0c47eff54a225921
kiritjs <kiritjogisingh@outlook.com>
2024-07-20 01:00:23 -0700

    scripted the health bar to player

---
+++ My project/Assets/HealthScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public GameObject player;
+    public Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.fillAmount=player.GetComponent<Player>().health/100f;
+    }
+}
